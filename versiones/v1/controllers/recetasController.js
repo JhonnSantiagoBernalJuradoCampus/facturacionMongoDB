72d644eb4b7eba24f52efd58a182b61a0dcb8d4c
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-09-05 11:47:49 -0500

    feat: :sparkles: Endpoint 9
    Get recetas prescritas por el Dr

--- versiones/v1/controllers/recetasController.js
+++ versiones/v1/controllers/recetasController.js
@@ -2,9 +2,15 @@ import { connectionDB } from "../../../db/conecct.js";
 
 const db = await connectionDB();
 const collection = db.collection("Recetas")
+
 const getRecetaEnero = async(req,res)=>{
     const result = await collection.find({"rec_fecha": {$gt: "2023-01-01"}}).toArray();
     res.send(result);
 };
 
-export {getRecetaEnero};
+const getRecetaDoctor = async (req,res)=>{
+    const result = await collection.find({nombre_empleado: {$eq: "Dr. Martinez"}}).toArray();
+    res.send(result);
+}
+
+export {getRecetaEnero, getRecetaDoctor};
--- versiones/v1/routes/recetaV1.routes.js
+++ versiones/v1/routes/recetaV1.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getRecetaEnero } from "../controllers/recetasController.js";
+import { getRecetaDoctor, getRecetaEnero } from "../controllers/recetasController.js";
 import { validatePermisos } from "../../../helpers/validatePermisos.js";
 
 const appReceta = Router();
 
 appReceta.get("/enero", validatePermisos("get"), getRecetaEnero);
+appReceta.get("/doctor", validatePermisos("get"), getRecetaDoctor);
 
 export default appReceta;
